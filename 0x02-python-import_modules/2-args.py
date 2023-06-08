1d7accdc86992324b9ae933acb37d361e2c49db4
khawla <boukniter.khawla@gmail.com>
2023-06-08 09:30:55 -0700

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-num = len(ys.argv[1:])
+num = len(sys.argv[1:])
 if num == 0:
     print("0 arguments.")
 elif num == 1:
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,4 +3,4 @@ from sys import argv
 sum = 0
 for i in argv[1:]:
     add += int(i)
-print("{}".format(sum))
+print("{:d}".format(sum))
