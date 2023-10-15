7c2f84e9989cb4f02440f3d803f37a3a3ad2d119
khawla <boukniter.khawla@gmail.com>
2023-10-15 06:04:50 -0700

    correct condition  by changing  alue < 0  by value><  0 infiles rectangle.py square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,11 +40,11 @@ class Square(Rectangle):
 
         Raises:
             TypeError: width must be an integer
-            ValueError: width must be >= 0.
+            ValueError: width must be > 0.
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
