d5d2dca0b51fa74dbbbafb148c7b55f5e55fbee6
khawla <boukniter.khawla@gmail.com>
2023-10-14 17:05:02 -0700

    fix pycodestyle file base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,7 +6,7 @@ import json
 class Base:
     """ Base class """
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """Initialize a new instance.
             Args:
@@ -20,7 +20,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ static method def to_json_string(list_dictionaries): 
+        """ static method def to_json_string(list_dictionaries):
         that returns the JSON string representation of list_dictionaries
         """
 
@@ -28,23 +28,22 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
-        
+
     @classmethod
     def save_to_file(cls, list_objs):
         """  writes the JSON string representation of list_objs to a file """
         l_obj = []
         filename = str(cls.__name__) + ".json"
         if list_objs is not None:
-            l_obj = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            l_o = cls.to_json_string([ob.to_dictionary() for ob in list_objs])
         with open(filename, 'w') as f:
-            f.write(l_obj)
-    
+            f.write(l_o)
+
     def from_json_string(json_string):
-        """ 
+        """
         that returns the list of the JSON string representation json_string
         """
         str_list = []
         if json_string is not None and len(json_string) > 0:
             str_list = json.loads(json_string)
         return (str_list)
-            
